7c8e377aed23e6700fbc412ed843d58e96cc94af
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-06-05 18:11:26 +0300

    Произвел вычесления для заданного числа

---
+++ script.js
@@ -0,0 +1,14 @@
+let num = 266219;
+
+let arr = num.toString().split(''); 
+
+let product = arr.reduce(function(previousValue, currentItem) { 
+    return previousValue * currentItem;
+});
+
+let inDegree = product ** 3; 
+
+let arr2 = inDegree.toString().split('');
+
+console.log(arr2[0]);
+console.log(arr2[1]);
