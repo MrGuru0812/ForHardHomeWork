2adaf75989e75d24240e0a0aeaa942682e3df4d6
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-06-08 18:43:33 +0300

    Решение двух задач

--- script.js
+++ script.js
@@ -1,15 +1,88 @@
-let num = 266219;
+'use strict'
+                                        //Первая задача.
+                                // Первый способ 
+let lang = 'ru';
 
-let arr = num.toString().split(''); 
+let weekDayRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let weekDayEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-let product = arr.reduce(function(previousValue, currentItem) { 
-    return previousValue * currentItem;
-});
+console.log('Первый способ');
 
-let inDegree = product ** 3; 
+let weekDay = function(lang) {
 
-let arr2 = inDegree.toString().split('');
 
-console.log(product);
-console.log(arr2[0]);
-console.log(arr2[1]);
+    if (lang === 'ru') {
+
+        for (let i = 0; i < weekDayRu.length; i++) {
+            
+            console.log(weekDayRu[i]);
+
+        };
+    
+    } else if (lang === 'en') {
+    
+        for (let i = 0; i < weekDayEn.length; i++) {
+            
+            console.log(weekDayEn[i]);
+
+    
+        };
+    
+    }
+
+};
+
+weekDay('en');
+console.log('Второй способ');
+
+let dayOfWeek = function(lang) {
+
+switch (lang) {
+    case 'ru':
+
+        for (let i = 0; i < weekDayRu.length; i++) {
+            
+            
+            console.log(weekDayRu[i]);
+        };
+        break;
+
+    case 'en':
+
+        for (let i = 0; i < weekDayEn.length; i++) {
+            
+            
+            console.log(weekDayEn[i]);
+    
+        };
+
+        break;
+    }
+};
+dayOfWeek('ru');
+
+
+
+
+console.log('Третий способ');
+
+let daysWeeks = [
+    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+];
+
+let language = (lang === 'ru') ? daysWeeks[0] : daysWeeks[1];
+for (let i = 0; i < language.length; i++) {
+
+    console.log(language[i]);
+}
+
+
+
+                                        //Вторая задача
+let namePerson = 'Михаил';
+
+let rank = (namePerson === 'Артем') ? console.log('директор') :
+(namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
+
+console.log(rank);
