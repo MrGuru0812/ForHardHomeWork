6bbfaa16e953aaffafc895d275e9ac7ed16061df
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-06-09 19:01:03 +0300

    реализовал определние типа данных строка

--- script.js
+++ script.js
@@ -1,88 +1,18 @@
 'use strict'
-                                        //Первая задача.
-                                // Первый способ 
-let lang = 'ru';
 
-let weekDayRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let weekDayEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+function stringFoo(param) {
 
-console.log('Первый способ');
-
-let weekDay = function(lang) {
-
-
-    if (lang === 'ru') {
-
-        for (let i = 0; i < weekDayRu.length; i++) {
-            
-            console.log(weekDayRu[i]);
-
-        };
+if(typeof param === 'string' && param.length > 30 || typeof param === 'string' ) {
+        
+    console.log(param.slice( 0, 39).trim() + '...');
     
-    } else if (lang === 'en') {
-    
-        for (let i = 0; i < weekDayEn.length; i++) {
-            
-            console.log(weekDayEn[i]);
+    } else {
 
-    
-        };
-    
+        console.log('Это не строка');
+        
     }
 
 };
 
-weekDay('en');
-console.log('Второй способ');
+stringFoo();
 
-let dayOfWeek = function(lang) {
-
-switch (lang) {
-    case 'ru':
-
-        for (let i = 0; i < weekDayRu.length; i++) {
-            
-            
-            console.log(weekDayRu[i]);
-        };
-        break;
-
-    case 'en':
-
-        for (let i = 0; i < weekDayEn.length; i++) {
-            
-            
-            console.log(weekDayEn[i]);
-    
-        };
-
-        break;
-    }
-};
-dayOfWeek('ru');
-
-
-
-
-console.log('Третий способ');
-
-let daysWeeks = [
-    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-];
-
-let language = (lang === 'ru') ? daysWeeks[0] : daysWeeks[1];
-for (let i = 0; i < language.length; i++) {
-
-    console.log(language[i]);
-}
-
-
-
-                                        //Вторая задача
-let namePerson = 'Михаил';
-
-let rank = (namePerson === 'Артем') ? console.log('директор') :
-(namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
-
-console.log(rank);
